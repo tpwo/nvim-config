25289c4cf711bac6585087e60a5e255af90d6703
Tomasz Wojdat <tpwojdat@gmail.com>
2026-06-07 14:24:28 +0200

    feat: replace mini.pick with telescope
    Bring picker configuration from my kickstart.nvim repo:
    https://github.com/tpwo/kickstart.nvim

--- init.lua
+++ init.lua
@@ -64,18 +64,44 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     callback = function() vim.hl.on_yank() end,
 })
 
+-- Build telescope-fzf-native automatically
+
+local function build_fzf(ev)
+    if ev.data.spec.name ~= "telescope-fzf-native.nvim" then
+        return
+    end
+
+    if ev.data.kind ~= "install" and ev.data.kind ~= "update" then
+        return
+    end
+
+    vim.system({ "make" }, { cwd = ev.data.path })
+end
+
+vim.api.nvim_create_autocmd("PackChanged", {
+    callback = build_fzf,
+})
+
 vim.pack.add({
+    -- Telescope with its deps and recommended fzf-native plugin
+    { src = "https://github.com/nvim-telescope/telescope.nvim" },
+    { src = "https://github.com/nvim-lua/plenary.nvim" },
+    { src = "https://github.com/nvim-telescope/telescope-fzf-native.nvim" },
+
     { src = 'https://github.com/christoomey/vim-tmux-navigator' },
-    { src = 'https://github.com/folke/tokyonight.nvim' },
+    { src = 'https://github.com/folke/todo-comments.nvim' },
     { src = 'https://github.com/folke/which-key.nvim' },
     { src = 'https://github.com/j-hui/fidget.nvim' },
     { src = 'https://github.com/mason-org/mason.nvim' },
     { src = 'https://github.com/mbbill/undotree' },
     { src = 'https://github.com/neovim/nvim-lspconfig' },
-    { src = 'https://github.com/nvim-mini/mini.pick',           version = 'stable' },
-    { src = 'https://github.com/nvim-mini/mini.statusline',     version = 'stable' },
+    { src = 'https://github.com/nvim-mini/mini.statusline',               version = 'stable' },
     { src = 'https://github.com/stevearc/oil.nvim' },
-    { src = 'https://github.com/folke/todo-comments.nvim' },
+
+    { src = 'https://github.com/folke/tokyonight.nvim' },
+})
+
+    },
 })
 
 -- Set mapped sequence wait time
@@ -95,13 +121,208 @@ require('mini.statusline').setup({ use_icons = false })
 require('oil').setup()
 require('todo-comments').setup({ signs = false })
 
-require('mini.pick').setup()
-local builtin = require('mini.pick').builtin
+-- Telescope
 
-vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = 'Pick open [B]uffers' })
-vim.keymap.set('n', '<leader>f', builtin.files, { desc = 'Pick [F]iles' })
-vim.keymap.set('n', '<leader>h', builtin.help, { desc = 'Pick [H]elp' })
-vim.keymap.set('n', '<leader>g', builtin.grep_live, { desc = 'Pick [G]rep' })
+require("telescope").setup({
+    extensions = {
+        fzf = {},
+    },
+})
+pcall(require("telescope").load_extension, "fzf")
+
+-- Telescope pickers
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+
+vim.keymap.set('n', '<leader>sf', function()
+    builtin.find_files { hidden = false }
+end, { desc = '[S]earch [F]iles' })
+
+-- Search in files including hidden and gitignored
+vim.keymap.set('n', '<leader>sF', function()
+    builtin.find_files {
+        hidden = true,
+        no_ignore = true,
+        no_ignore_parent = true,
+    }
+end, { desc = '[S]earch [F]iles (Hidden)' })
+
+-- Search in files including hidden and gitignored
+vim.keymap.set('n', '<leader>sA', function()
+    builtin.find_files {
+        hidden = true,
+        no_ignore = true,
+        no_ignore_parent = true,
+    }
+end, { desc = '[S]earch *[A]ll* files' })
+
+vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+vim.keymap.set({ 'n', 'v' }, '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+
+vim.keymap.set('n', '<leader>sg', function()
+    builtin.live_grep {
+        additional_args = function()
+            -- Pass rgrip options here
+            return { '--hidden' }
+        end,
+    }
+end, { desc = '[S]earch by [G]rep' })
+
+vim.keymap.set('n', '<leader>sd', function()
+    builtin.diagnostics {
+        root_dir = vim.fn.getcwd(),
+    }
+end, { desc = '[S]earch [D]iagnostics' })
+
+vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+vim.keymap.set('n', '<leader>sc', builtin.commands, { desc = '[S]earch [C]ommands' })
+vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+
+-- This runs on LSP attach per buffer (see main LSP attach function in 'neovim/nvim-lspconfig' config for more info,
+-- it is better explained there). This allows easily switching between pickers if you prefer using something else!
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('telescope-lsp-attach', { clear = true }),
+    callback = function(event)
+        local buf = event.buf
+
+        -- Find references for the word under your cursor.
+        vim.keymap.set('n', 'grr', builtin.lsp_references, { buffer = buf, desc = '[G]oto [R]eferences' })
+
+        -- Jump to the implementation of the word under your cursor.
+        -- Useful when your language has ways of declaring types without an actual implementation.
+        vim.keymap.set('n', 'gri', builtin.lsp_implementations, { buffer = buf, desc = '[G]oto [I]mplementation' })
+
+        -- Jump to the definition of the word under your cursor.
+        -- This is where a variable was first declared, or where a function is defined, etc.
+        -- To jump back, press <C-t>.
+        vim.keymap.set('n', 'grd', builtin.lsp_definitions, { buffer = buf, desc = '[G]oto [D]efinition' })
+
+        -- Fuzzy find all the symbols in your current document.
+        -- Symbols are things like variables, functions, types, etc.
+        vim.keymap.set('n', 'gO', builtin.lsp_document_symbols, { buffer = buf, desc = 'Open Document Symbols' })
+
+        -- Fuzzy find all the symbols in your current workspace.
+        -- Similar to document symbols, except searches over your entire project.
+        vim.keymap.set('n', 'gW', builtin.lsp_dynamic_workspace_symbols,
+            { buffer = buf, desc = 'Open Workspace Symbols' })
+
+        -- Jump to the type of the word under your cursor.
+        -- Useful when you're not sure what type a variable is and you want to see
+        -- the definition of its *type*, not where it was *defined*.
+        vim.keymap.set('n', 'grt', builtin.lsp_type_definitions, { buffer = buf, desc = '[G]oto [T]ype Definition' })
+    end,
+})
+
+-- Override default behavior and theme when searching
+vim.keymap.set('n', '<leader>/', function()
+    -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+    builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+        winblend = 10,
+        previewer = false,
+    })
+end, { desc = '[/] Fuzzily search in current buffer' })
+
+-- It's also possible to pass additional configuration options.
+--  See `:help telescope.builtin.live_grep()` for information about particular keys
+vim.keymap.set(
+    'n',
+    '<leader>s/',
+    function()
+        builtin.live_grep {
+            grep_open_files = true,
+            prompt_title = 'Live Grep in Open Files',
+        }
+    end,
+    { desc = '[S]earch [/] in Open Files' }
+)
+
+-- Shortcut for searching your Neovim configuration files
+vim.keymap.set('n', '<leader>sn', function()
+    builtin.find_files { cwd = vim.fn.stdpath 'config' }
+end, { desc = '[S]earch [N]eovim files' })
+
+-- Toggle pickers
+vim.keymap.set('n', '<leader>tf', builtin.filetypes, { desc = '[T]oggle [F]iletype' })
+
+-- Git pickers
+
+-- Helper function checking if we're in a Git repo. Without it, telescope Git pickers fail with a loud error
+local function require_git_repo(fn)
+    return function()
+        local git_root = vim.fn.system('git rev-parse --show-toplevel 2>/dev/null'):gsub('\n', '')
+        if git_root == '' then
+            vim.notify('Not in a git repository', vim.log.levels.ERROR)
+            return
+        end
+        fn()
+    end
+end
+
+vim.keymap.set('n', '<leader>gs', require_git_repo(builtin.git_status), { desc = '[G]it [S]tatus' })
+
+vim.keymap.set(
+    'n',
+    '<leader>gf',
+    require_git_repo(function()
+        builtin.find_files {
+            prompt_title = 'Git Files',
+            find_command = {
+                'git',
+                'ls-files',
+                '--exclude-standard',
+                '--cached',
+                vim.fn.getcwd(),
+            },
+        }
+    end),
+    { desc = '[G]it [F]iles' }
+)
+
+vim.keymap.set('n', '<leader>gF', require_git_repo(builtin.git_files), { desc = '[G]it [F]iles (All)' })
+
+vim.keymap.set('n', '<leader>gb', require_git_repo(builtin.git_branches), { desc = '[G]it [B]ranches' })
+
+-- TODO: I want to see commit author and date in this output
+vim.keymap.set('n', '<leader>gc', require_git_repo(builtin.git_commits), { desc = '[G]it [C]ommits' })
+
+vim.keymap.set(
+    'n',
+    '<leader>gh',
+    require_git_repo(function()
+        builtin.git_bcommits { prompt_title = 'Git File History' }
+    end),
+    { desc = '[G]it file [H]istory' }
+)
+
+vim.keymap.set(
+    'n',
+    '<leader>gl',
+    require_git_repo(function()
+        builtin.git_bcommits_range { prompt_title = 'Git Line History' }
+    end),
+    { desc = '[G]it [L]ine history' }
+)
+
+-- Dotfiles picker
+-- Configured to work with: https://github.com/tpwo/dotfiles
+vim.keymap.set('n', '<leader>sD', function()
+    builtin.find_files {
+        prompt_title = 'Dotfiles',
+        cwd = vim.fn.expand '~/ws/private/repos/tpwo/dotfiles/dotfiles',
+        find_command = {
+            'git',
+            'ls-files',
+            '--exclude-standard',
+            '--cached',
+        },
+    }
+end, { desc = '[S]earch [D]otfiles' })
+
+-- End of Telescope pickers
+
 
 -- Disable showmode, as statusline contains it
 vim.o.showmode = false
